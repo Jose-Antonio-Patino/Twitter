29835437ec3165d02cc8cf8f7ce2f74bb6fa5334
Jose Patino <jose.patino88@icloud.com>
2020-02-06 09:42:37 -0500

    Tweet Cell Design

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class HomeTableViewController: UITableViewController
 {
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
     }
 
     @IBAction func onLogOut(_ sender: Any)
@@ -24,20 +23,29 @@ class HomeTableViewController: UITableViewController
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetTableViewCell
+            
+        cell.userNameLabel.text = "SomeName"
+        cell.tweetContent.text = "Something else"
+        
+        
+        return cell
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int
     {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // only one section for this project
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // number of tweets
+        return 1
     }
-
- 
 
 }
